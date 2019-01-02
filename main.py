5d6832b43ebb32a83b2a2c6224a8e365dae4fcde
momotran <46303685+momotran@users.noreply.github.com>
2019-01-02 14:50:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ async def init_activity():
   await bot.change_presence(activity=activity)
 
 async def log(content,use_time=True):
-	print(content)
-  channel=utils.config['log_channel']
+  print(content)
+  channel=None
   if channel==None:
     print('Error: Not found log channel.')
     return
@@ -26,29 +26,29 @@ async def log(content,use_time=True):
 
 @bot.event
 async def on_ready():
-    #init_variables()
-    await init_activity()
-    msg=(
-        '```'
-        f'version: {str(datetime.datetime.now())}\n'
-        f'discord.py: {discord.__version__}\n'
-        '```'
-        )
-    await log(msg,use_time=False)
+  #init_variables()
+  await init_activity()
+  msg=(
+    '```'
+    f'version: {str(datetime.datetime.now())}\n'
+    f'discord.py: {discord.__version__}\n'
+    '```'
+  )
+  await log(msg,use_time=False)
 
 @bot.event
 async def on_message(message):
-    guild, channel, sender, msg = message.guild, message.channel, message.author, message. content
-    if sender.id!=bot.user.id:
-        if len(message.mentions)==1 and bot.user.mentioned_in(message):
-            async with channel.typing():
-                delay_time=int(random.choice(range(40, 100))) / 100.0
-                #rely=utils.to_emoji(logic.req_to_res(msg))
-                await asyncio.sleep(delay_time)
-                #await channel.send(f'{sender.mention} {rely}')
-        if channel.id!=utils.config['log_channel_id']:
-            await log(f'[{guild.name}][{channel.name}]{sender.name}\n{msg}')
-    if msg.startswith(prefix_char):
-        await bot.process_commands(message)
+  guild, channel, sender, msg = message.guild, message.channel, message.author, message. content
+  if sender.id!=bot.user.id:
+    if len(message.mentions)==1 and bot.user.mentioned_in(message):
+      async with channel.typing():
+        delay_time=int(random.choice(range(40, 100))) / 100.0
+        #rely=utils.to_emoji(logic.req_to_res(msg))
+        await asyncio.sleep(delay_time)
+        #await channel.send(f'{sender.mention} {rely}')
+    if channel.id!=utils.config['log_channel_id']:
+      await log(f'[{guild.name}][{channel.name}]{sender.name}\n{msg}')
+  if msg.startswith(prefix_char):
+    await bot.process_commands(message)
 
 bot.run(token)
