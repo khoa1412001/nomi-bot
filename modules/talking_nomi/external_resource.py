5a223195d0f62fb035d6fac14cca295c6f8c26b7
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-07-31 14:29:15 +0700

    Update external_resource.py

--- modules/talking_nomi/external_resource.py
+++ modules/talking_nomi/external_resource.py
@@ -29,3 +29,13 @@ def read_request_to_response():
     key = str_split[0]
     value = str_split[1].split('||')
     talking_nomi.req_to_res[key] = value
+
+def read_similars():
+  f = open('modules/talking_nomi/resources/similars.nomi', 'r')
+  lines = f.readlines()
+  f.close()
+  for line in lines:
+    str_split = line.split('::')
+    key = str_split[0]
+    value = str_split[1].split('||')
+    talking_nomi.all_similars[key] = value
