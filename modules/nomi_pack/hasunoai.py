a794b5c09db57c6f37d36463633f52019ad184d2
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-08-02 13:11:05 +0700

    Create hasunoai.py

---
+++ modules/nomi_pack/hasunoai.py
@@ -0,0 +1,18 @@
+import random 
+
+urls = []
+
+def load():
+    f = open('resources/hasunoai_pack.nomi', 'r')
+    lines = f.readlines()
+    f.close()
+    global urls
+    for line in lines:
+        url = line[:-1]
+        urls.append(url)
+
+def get_random():
+    global urls
+    return random.choice(urls)
+
+load()
