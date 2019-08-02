d862f382181c69f26c4e77ffe80711cc5303bdd9
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-08-02 13:09:56 +0700

    Create hanime.py

---
+++ modules/nomi_pack/hanime.py
@@ -0,0 +1,18 @@
+import random 
+
+urls = []
+
+def load():
+    f = open('resources/hanime_pack.nomi', 'r')
+    lines = f.readlines()
+    f.close()
+    global urls
+    for line in lines:
+        url = line[:-1]
+        urls.append(url)
+
+def get_one():
+    global urls
+    return random.choice(urls)
+
+load()
