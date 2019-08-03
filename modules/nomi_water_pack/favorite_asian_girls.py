06ff08137ecb9a930b5cf715c8a7ed3c8f6e5361
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-08-03 21:16:45 +0700

    Create favorite_asian_girls.py

---
+++ modules/nomi_water_pack/favorite_asian_girls.py
@@ -0,0 +1,18 @@
+import random 
+
+urls = []
+
+def load():
+    f = open('modules/nomi_water_pack/resources/favorite_asian_girls.nomi', 'r')
+    lines = f.readlines()
+    f.close()
+    global urls
+    for line in lines:
+        url = line[:-1]
+        urls.append(url)
+
+def get_random():
+    global urls
+    return random.choice(urls)
+
+load()
