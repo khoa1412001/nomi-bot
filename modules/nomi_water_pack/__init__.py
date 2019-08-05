1e6bf627eddbfd41f67846512f49901d1e8c6971
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-08-05 19:51:57 +0700

    Update __init__.py

--- modules/nomi_water_pack/__init__.py
+++ modules/nomi_water_pack/__init__.py
@@ -1,1 +1,20 @@
-from modules.nomi_water_pack import hanime, hasunoai, ulzzang_face, favorite_asian_girls, instababes_asian
+packs = {}
+
+def load(name):
+  f = open(f'modules/nomi_water_pack/resources/{name}.nomi', 'r')
+  urls = f.readlines()
+  f.close()
+  global packs
+  packs[name] = []
+  for url in urls:
+    packs[name].append(url[:-1])
+
+def get_random(name):
+  global packs
+  random.choice(packs[name])
+ 
+load('hasunoai')
+load('hanime')
+load('ulzzang_face')
+load('favorite_asian_girls')
+load('instababes.asian')
