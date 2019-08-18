38926919819f855f9a3ed3fc0f5ac8f03b944ccf
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-08-18 16:40:19 +0700

    Update nomi-host-data.py

--- servers/nomi-host-data.py
+++ servers/nomi-host-data.py
@@ -34,28 +34,6 @@ class NomiHostData(commands.Cog):
   @commands.Cog.listener()
   async def on_message(self, message):
     if (message.author.id != self.bot.user.id):
-      if (len(message.mentions) == 1) and (self.bot.user.mentioned_in(message)):
-        async with message.channel.typing():
-          delay_time = random.choice(range(45, 100)) / 100.0
-          await asyncio.sleep(delay_time)
-        logic = chitchat.Logic(message.content)
-        req, res, cou = logic.request, logic.response, logic.counts
-        if (res == 'not found response'):
-          str = (
-            '```[ERROR]```'
-            f'No response found.\n'
-            f'request: {req}\n'
-          )
-          await self.my_channels['talking'].send(str)
-        else:
-          await message.channel.send(f'{message.author.mention} {res}')
-          str = (
-            '```[DEBUG]```'
-            f'request: {req}\n'
-            f'response: {res}\n'
-            f'counts: {cou}\n'
-          )
-          await self.my_channels['talking'].send(str)
       if 'messages' in self.my_channels:
         str = (
           '```[MESSAGE]```'
