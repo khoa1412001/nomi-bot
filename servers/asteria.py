9474d718d58a53509bbd63875f87067b0ba4c0b0
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-08-18 09:01:34 +0700

    Update asteria.py

--- servers/asteria.py
+++ servers/asteria.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from modules import nomi_water_pack
-import discord, asyncio, random
+import discord, asyncio
 
 class Asteria(commands.Cog):
   def __init__(self, bot):
@@ -21,35 +21,32 @@ class Asteria(commands.Cog):
   async def send_packs(self):
     await self.bot.wait_until_ready()
     while (True):
-      image_url = nomi_water_pack.get_random('hasunoai')
+      image_url = nomi_water_pack.o.get_random(['hasunoai'])
       embed = discord.Embed()
       embed.set_image(url = image_url)
       await self.my_channels['jap-cos'].send(embed = embed)
 
-      image_url = nomi_water_pack.get_random('hanime')
+      image_url = nomi_water_pack.o.get_random(['hanime'])
       embed = discord.Embed()
       embed.set_image(url = image_url)
       await self.my_channels['hentai-pic'].send(embed = embed)
 
-      image_urls = [nomi_water_pack.get_random('ulzzang_face'), nomi_water_pack.get_random('ulzzang__girlz')]
-      image_url = random.choice(image_urls)
+      image_url = nomi_water_pack.o.get_random(['ulzzang_face', 'ulzzang__girlz'])
       embed = discord.Embed()
       embed.set_image(url = image_url)
       await self.my_channels['korean'].send(embed = embed)
 
-      image_urls = [nomi_water_pack.get_random('favorite_asian_girls'), nomi_water_pack.get_random('instababes.asian'), nomi_water_pack.get_random('69pretty.official')]
-      image_url = random.choice(image_urls)
+      image_url = nomi_water_pack.o.get_random(['favorite_asian_girls', 'instababes.asian', '69pretty.official'])
       embed = discord.Embed()
       embed.set_image(url = image_url)
       await self.my_channels['mixed'].send(embed = embed)
 
-      image_urls = [nomi_water_pack.get_random('vietnamesexybabe'), nomi_water_pack.get_random('vneseg'), nomi_water_pack.get_random('angels.in.vn'), nomi_water_pack.get_random('girl_xinh')]
-      image_url = random.choice(image_urls)
+      image_url = nomi_water_pack.o.get_random(['vietnamesexybabe', 'vneseg', 'angels.in.vn', 'girl_xinh'])
       embed = discord.Embed()
       embed.set_image(url = image_url)
       await self.my_channels['vn'].send(embed = embed)
       
-      image_url = nomi_water_pack.get_random('hoingamgaitay')
+      image_url = nomi_water_pack.get_random('[hoingamgaitay]')
       embed = discord.Embed()
       embed.set_image(url = image_url)
       await self.my_channels['western'].send(embed = embed)
