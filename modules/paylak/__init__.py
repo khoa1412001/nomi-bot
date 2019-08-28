878aafc3ab5a185f44cf7175d4d0ef177709b202
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-08-28 15:35:07 +0700

    Update __init__.py

--- modules/paylak/__init__.py
+++ modules/paylak/__init__.py
@@ -35,8 +35,7 @@ class Song(discord.PCMVolumeTransformer):
     if 'entries' in data:
       data = data['entries'][0]
     filename = data['url'] if stream else ytdl.prepare_filename(data)
-    print(data)
-    return cls(discord.FFmpegPCMAudio(filename, options = '-vn'), volume = volume, title = data['title'], url = data['url'], duration = 5)
+    return cls(discord.FFmpegPCMAudio(filename, options = '-vn'), volume = volume, title = data['title'], url = url, duration = data['duration'])
 
 class MusicGuild():
   id = None
