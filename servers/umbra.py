f092683f6e41e7fea1e2f9e8e45a9e8ae290d8ac
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-09-29 09:08:42 +0700

    Update umbra.py

--- servers/umbra.py
+++ servers/umbra.py
@@ -58,40 +58,6 @@ class Umbra(commands.Cog):
       await self.my_channels['mixed'].send(embed = embed)
       
       await asyncio.sleep(5400)
-      
- async def print_last_online(self):
-    await self.bot.wait_until_ready()
-    while (True):
-      now = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-      if (now.hour in [0, 12] and now.minute in [0]):
-        members = self.my_guild.members
-        str = ''
-        for member in members: 
-          if member.bot:
-            continue
-          if (member.id not in self.last_online):
-            self.last_online[member.id] = 'No information'
-          str += f'{member.name}#{member.discriminator} ({member.display_name}) : {self.last_online[member.id]}\n'
-        f = open('temp.txt', 'w')
-        f.write(str)
-        f.close()
-        f = open('temp.txt', 'rb')
-        file = discord.File(
-          fp = f,
-          filename = 'online-status.txt'
-        )
-        await self.my_channels['online-status'].send(file = file)
-        f.close()
-        os.remove('temp.txt')
-        await asyncio.sleep(60)
-      else:
-        await asyncio.sleep(30)
-    
-  @commands.Cog.listener()
-  async def on_member_update(self, before, after):
-    if (after.guild == self.my_guild):
-      if (after.status != discord.Status.offline):
-        self.last_online[after.id] = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%Y-%m-%d %H:%M:%d")
         
   @commands.Cog.listener()
   async def on_ready(self):
