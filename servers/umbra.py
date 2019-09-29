81f85b691312a35a8c8fbde65e06ebd7c38b0d7b
Gia An <46303685+tarngaina@users.noreply.github.com>
2019-09-29 08:58:35 +0700

    Update umbra.py

--- servers/umbra.py
+++ servers/umbra.py
@@ -7,7 +7,6 @@ class Umbra(commands.Cog):
     self.bot = bot
     self.my_guild = None
     self.my_channels = {}
-    self.my_roles = {}
     self.last_online = {}
 
   def find_objects(self):
@@ -18,53 +17,7 @@ class Umbra(commands.Cog):
         channels = guild.channels
         for channel in channels:
           self.my_channels[channel.name] = channel
-        roles = guild.roles
-        for role in roles:
-          self.my_roles[role.name] = role
         break
-        
-  async def print_last_online(self):
-    await self.bot.wait_until_ready()
-    while (True):
-      now = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-      if (now.hour in [0, 12] and now.minute in [0]):
-        members = self.my_guild.members
-        str = ''
-        for member in members: 
-          if member.bot:
-            continue
-          if (member.id not in self.last_online):
-            self.last_online[member.id] = 'No information'
-          str += f'{member.name}#{member.discriminator} ({member.display_name}) : {self.last_online[member.id]}\n'
-        f = open('temp.txt', 'w')
-        f.write(str)
-        f.close()
-        f = open('temp.txt', 'rb')
-        file = discord.File(
-          fp = f,
-          filename = 'online-status.txt'
-        )
-        await self.my_channels['online-status'].send(file = file)
-        f.close()
-        os.remove('temp.txt')
-        await asyncio.sleep(60)
-      else:
-        await asyncio.sleep(30)
-
-  async def daily_time_handle(self):
-    await self.bot.wait_until_ready()
-    while (True):
-      now = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-      if (now.hour in [17] and now.minute in [55]):
-        str = f'{self.my_roles["Member"].mention}, Urus in next 5 minutes.'
-        await self.my_channels['kms-daily'].send(str)
-        await asyncio.sleep(60)
-      elif (now.hour in [9, 16, 18] and now.minute in [55]):
-        str = f'{self.my_roles["Member"].mention}, Flag Race in next 5 minutes.'
-        await self.my_channels['kms-daily'].send(str)
-        await asyncio.sleep(60)
-      else:
-        await asyncio.sleep(30)
 
   async def send_packs(self):
     await self.bot.wait_until_ready()
@@ -105,20 +58,47 @@ class Umbra(commands.Cog):
       await self.my_channels['mixed'].send(embed = embed)
       
       await asyncio.sleep(5400)
-
-  @commands.Cog.listener()
-  async def on_ready(self):
-    self.find_objects()
-    loop = asyncio.get_event_loop()
-    loop.create_task(self.daily_time_handle())
-    loop.create_task(self.send_packs())
-    loop.create_task(self.print_last_online())
+      
+ async def print_last_online(self):
+    await self.bot.wait_until_ready()
+    while (True):
+      now = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
+      if (now.hour in [0, 12] and now.minute in [0]):
+        members = self.my_guild.members
+        str = ''
+        for member in members: 
+          if member.bot:
+            continue
+          if (member.id not in self.last_online):
+            self.last_online[member.id] = 'No information'
+          str += f'{member.name}#{member.discriminator} ({member.display_name}) : {self.last_online[member.id]}\n'
+        f = open('temp.txt', 'w')
+        f.write(str)
+        f.close()
+        f = open('temp.txt', 'rb')
+        file = discord.File(
+          fp = f,
+          filename = 'online-status.txt'
+        )
+        await self.my_channels['online-status'].send(file = file)
+        f.close()
+        os.remove('temp.txt')
+        await asyncio.sleep(60)
+      else:
+        await asyncio.sleep(30)
     
   @commands.Cog.listener()
   async def on_member_update(self, before, after):
     if (after.guild == self.my_guild):
       if (after.status != discord.Status.offline):
         self.last_online[after.id] = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%Y-%m-%d %H:%M:%d")
+        
+  @commands.Cog.listener()
+  async def on_ready(self):
+    self.find_objects()
+    loop = asyncio.get_event_loop()
+    loop.create_task(self.send_packs())
+    loop.create_task(self.print_last_online())
 
 def setup(bot):
   bot.add_cog(Umbra(bot))
